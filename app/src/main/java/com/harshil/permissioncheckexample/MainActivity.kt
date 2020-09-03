d536f9dafb031165b7523cb48dc0c015b1d94518
harshsquad206 <harshsquad206@gmail.com>
2020-09-03 14:06:21 +0530

    Final Commit

--- app/src/main/java/com/harshil/permissioncheckexample/MainActivity.kt
+++ app/src/main/java/com/harshil/permissioncheckexample/MainActivity.kt
@@ -16,18 +16,25 @@ class MainActivity : AppCompatActivity() {
 
     fun onAsk(view: View) {
         PermissionCheck.with(this@MainActivity)
+            //Pass all the permission which you want to access here
             .setPermissions(
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 android.Manifest.permission.READ_EXTERNAL_STORAGE,
                 android.Manifest.permission.CAMERA,
                 android.Manifest.permission.RECORD_AUDIO
             )
+            // If you don't want to check user has given permission or not then add this
+            // otherwise don't add. Default it will check user has granted permission or not
             .skipDenyAndForceDeny()
+            // when user accept all permission
+            // it return which permission is granted by user
             .onAccepted {
                 Toast.makeText(this@MainActivity, "Set all the permission", Toast.LENGTH_SHORT)
                     .show()
                 Log.d(javaClass.simpleName, "onAccepted: $it ")
             }
+            // when user deny for any one permission among all
+            // it return which permission is denied by user
             .onDenied {
                 Toast.makeText(
                     this@MainActivity,
@@ -36,6 +43,8 @@ class MainActivity : AppCompatActivity() {
                 ).show()
                 Log.d(javaClass.simpleName, "onDenied: $it")
             }
+            // when user force deny for one permission among all
+            // it will return which permission is force denied by user
             .onForeverDenied {
                 Toast.makeText(
                     this@MainActivity,
@@ -44,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                 ).show()
                 Log.d(javaClass.simpleName, "onForeverDenied: $it")
             }
+            // Don't forgot to call ask() for asking permission
             .ask()
     }
 }
